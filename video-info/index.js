b107f0ab8b60ffc2c38a78f4c961f44d417c2057
Jasenpan1987 <jasenpan@gmail.com>
2017-10-24 23:51:25 +1100

    video duration app complete

--- video-info/index.js
+++ video-info/index.js
@@ -1,7 +1,27 @@
 const electron = require("electron");
+const ffmpeg = require("fluent-ffmpeg");
 
-const { app } = electron; // overall running electron app process
+// electron ipc:
+// From window to Electron App
+// mainWindow   (ipcRenderer.send) --------------------> (ipcMain.on)     Electron App
+// From App to Electron window
+// Electron App (mainWindow.webContents.send) ---------> (ipcRenderer.on) mainWindow
+
+
+const { app, BrowserWindow, ipcMain } = electron; // overall running electron app process
+
+let mainWindow;
 
 app.on("ready", () => { // event based
-  console.log("app is ready");
+  mainWindow = new BrowserWindow({});
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+});
+
+// register event listener
+ipcMain.on("video:submit", (event, filePath) => {
+  ffmpeg.ffprobe(filePath, (error, meta) => {
+    // console.log(meta.format.duration);
+    mainWindow && mainWindow.webContents.send("video:duration", meta.format.duration);
+  })
 });
